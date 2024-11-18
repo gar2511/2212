0d1485508a507d9436ee71e8ce7702b3ff8eefe8
Gary Gao <ggao44@uwo.ca>
2024-11-18 17:32:56 -0500

    commit

--- src/main/java/com/example/controller/GameController.java
+++ src/main/java/com/example/controller/GameController.java
@@ -1,4 +1,19 @@
 package com.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class GameController {
+
+    public GameController() {
+        System.out.println("GameController initialized");
+    }
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private void goBack() {
+        SceneController.getInstance().switchToMainMenu();
+    }
 }
